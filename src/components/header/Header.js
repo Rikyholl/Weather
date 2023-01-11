26f76c47aa9b0193f52febf0f5f0f2dbfb3f8108
Rikyholl <rikyhollundead1234@gmail.com>
2023-01-11 22:11:50 +0500

    done change theme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import Logo from "../../assets/images/icons/logo";
 import Theme from "../../assets/images/icons/theme";
 import Select from 'react-select';
+import { useEffect, useState } from "react";
 
 const Header = () => {
 
@@ -10,18 +11,50 @@ const options = [
     { value: 'Ekaterinburg', label: 'Ekaterinburg' }
 ]
 
+const [theme, setTheme] = useState('light');
+
 const colourStyles = {
     control: (styles) => ({
         ...styles,
-        backgroundColor: 'rgba(71, 147, 255, 0.2)',
+        backgroundColor: theme === 'dark' ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
         width: '194px',
         height: '37px',
         border: 'none',
         borderRadius: '10px',
         zIndex: 100,
         marginLeft: '20px'
-    })
+    }),
+    singleValue: (styles) => ({
+        ...styles,
+        color: theme === 'dark' ? '#fff' : '#000',
+    }),
+};
+
+
+function changeTheme(){
+    setTheme(theme === 'light' ? 'dark' : 'light');
 }
+
+useEffect(() => {
+
+    const root = document.querySelector(':root');
+
+    const components = [
+        'bg-default', 
+        'component-bg', 
+        'card-bg', 
+        'color'
+    ];
+
+    root.style.setProperty('--bg-default', `var(--bg-${theme})`);
+
+    components.forEach((component) => {
+        root.style.setProperty(
+            `--${component}-default`, 
+            `var(--${component}-${theme})`
+        );
+    })
+}, [theme])
 
  return(
 
@@ -33,7 +66,14 @@ const colourStyles = {
         </div>
 
         <div className="header_wrapper">
-            <Theme />
+
+            <div 
+                onClick={changeTheme}
+                className="change_theme"
+            >
+                <Theme />
+            </div>
+            
             <Select 
                 defaultValue={options[0]} 
                 styles={colourStyles} 
